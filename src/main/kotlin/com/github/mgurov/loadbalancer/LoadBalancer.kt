ccc14959fc65814d3895042265c1558f720b193c
Mykola Gurov <ngurov@gmail.com>
2020-07-11 17:15:56 +0200

    step3: random provider balancing strategy

--- src/main/kotlin/com/github/mgurov/loadbalancer/LoadBalancer.kt
+++ src/main/kotlin/com/github/mgurov/loadbalancer/LoadBalancer.kt
@@ -1,7 +1,10 @@
 package com.github.mgurov.loadbalancer
 
+import kotlin.random.Random
+
 class LoadBalancer(
-        val capacity: Int = 10 //max number of providers allowed to be registered
+        val capacity: Int = 10, //max number of providers allowed to be registered
+        val balancingStrategy: BalancingStrategy = RandomBalancingStrategy()
 ) {
     private val providers: MutableList<Provider> = mutableListOf() //TODO: thread unsafe yet
     fun register(provider: Provider) {
@@ -9,5 +12,21 @@ class LoadBalancer(
             "Can't register more than $capacity providers"
         }
         providers += provider //TODO: thread safety
+    }
+
+    fun get(): String {
+        return balancingStrategy.selectNext(providers).get()
+    }
+}
+
+interface BalancingStrategy {
+    fun selectNext(providers: List<Provider>): Provider
+}
+
+class RandomBalancingStrategy(
+        private val random: Random = Random.Default
+): BalancingStrategy {
+    override fun selectNext(providers: List<Provider>): Provider {
+        return providers[random.nextInt(providers.size)] //TODO: thread unsafe re. random
     }
 }
--- src/test/kotlin/com/github/mgurov/loadbalancer/LoadBalancerTest.kt
+++ src/test/kotlin/com/github/mgurov/loadbalancer/LoadBalancerTest.kt
@@ -1,18 +1,31 @@
 package com.github.mgurov.loadbalancer
 
 import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.SoftAssertions
 import org.junit.jupiter.api.Test
+import kotlin.random.Random
 
 class LoadBalancerTest {
     @Test
     fun `should reject providers exceeding capacity`() {
-        //given
         val loadBalancer = LoadBalancer(capacity = 2)
         loadBalancer.register(Provider("first provider"))
         loadBalancer.register(Provider("second provider"))
-        //when-then
+
         Assertions.assertThatIllegalStateException().isThrownBy {
-            loadBalancer.register(Provider("exceeding the capacity of 2"))
+            loadBalancer.register(Provider("third provides exceeds the capacity of 2"))
         }
+    }
+
+    @Test
+    fun `should call random providers when configured with such strategy`() {
+        val loadBalancer = LoadBalancer(balancingStrategy = RandomBalancingStrategy(Random(0L)))
+        loadBalancer.register(Provider("1"))
+        loadBalancer.register(Provider("2"))
+
+        val actuals = (1..10).map { loadBalancer.get() }
+
+        assertThat(actuals).containsExactly("2", "1", "2", "2", "2", "1", "1", "1", "1", "2")
     }
 }
