5135da3047b415c2f4d5799f0da9ce527d0dcd8c
Mykola Gurov <ngurov@gmail.com>
2020-07-14 19:43:32 +0200

    fine

--- src/main/kotlin/com/github/mgurov/loadbalancer/LoadBalancer.kt
+++ src/main/kotlin/com/github/mgurov/loadbalancer/LoadBalancer.kt
@@ -168,44 +168,5 @@ class LoadBalancer(
     }
 }
 
-/**
- * Implementation notes:
- *
- * If provided as a library with little control over the clients of the LoadBalancer,
- * we might want to hide this interface so that it's signature isn't a part of the public contract should we want to implement
- * more elaborate strategies in the future that require more knowledge about the active options number.
- *
- */
-interface BalancingStrategy {
-    /**
-     * selectNextIndex should return the 0-based index of the next option (Provider) based on the strategy.
-     *
-     * The strategy may keep a state between the invocations.
-     *
-     * The invocations are supposed to be serialized by the caller, e.g. no two simultaneous calls would be made from different threads.
-     */
-    fun selectNextIndex(optionsCount: Int): Int
-}
-
-class RandomBalancingStrategy(
-        private val randomSupplier: () -> Random = ThreadLocalRandom::current
-): BalancingStrategy {
-    override fun selectNextIndex(optionsCount: Int): Int {
-        return randomSupplier().nextInt(optionsCount)
-    }
-}
-
-//TODO: unit-test edge cases.
-// a simplified RoundRobin that doesn't track the changes in the list of the available nodes
-class RoundRobinBalancingStrategy(
-        private var position: Int = 0
-): BalancingStrategy {
-    override fun selectNextIndex(optionsCount: Int): Int {
-        val theNextOne = position % optionsCount
-        position = (position + 1) % optionsCount
-        return theNextOne
-   }
-}
-
 //TODO: mention potential performance benefit of lambda's
 //TODO: read on the thread barriers.
