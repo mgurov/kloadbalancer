202922b4af38afeb88ee6948614113a984f0e806
Mykola Gurov <ngurov@gmail.com>
2020-07-14 19:39:16 +0200

    ok

--- src/test/kotlin/com/github/mgurov/loadbalancer/LoadBalancerTest.kt
+++ src/test/kotlin/com/github/mgurov/loadbalancer/LoadBalancerTest.kt
@@ -1,6 +1,5 @@
 package com.github.mgurov.loadbalancer
 
-import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatIllegalStateException
 import org.junit.jupiter.api.Test
